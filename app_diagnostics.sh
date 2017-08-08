38936b65988beef71ad00a210e631962625aa3b9
JeepGuy <jcbrent@ieee.org>
2017-08-08 08:41:36 -0400

    Add a first version of a new script

---
+++ app_diagnostics.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# The above line is called shebang.
+# Author: jcbrent
+# Created Date: <yyy-mm-dd>
+# Modified by: <Comma Delimited list>
+#   < add to the Modified by list so we know who modified it last.
+# <Replace with the description and/or purpose of this shell script.>
+# ------------------------------------------------------------------------------------
+
+# Set Global Variables first  so they are always defined before being called.
+#------------- Sample below
+GLOBAL_VAR1="one"
+GLOBAL_VAR2="two"
+
+
+# Create Functions second - so they are read into the current bash session second
+#------------- Sample below
+function function_one() {
+  local LOCAL_VAR1="one"
+  # <Replace with function code.>
+  echo "This script called the sample function (function_one) which does nothing (except print this line)."
+}
+
+echo " "
+echo " Starting the \"Application Diagnostic Script/\" Script  - Named:  \"app_diagnostics.sh "
+echo " ----------------------------------------------------------------------------- "
+echo " "
+echo " This script runs several checks on the applicaton to detemine the current status of the tool/application"
+echo " "
+
+# Main body of the shell script starts here.
+#
+# <Replace with the main command comments of your shell script.>
+
+df -h
+echo " "
+echo " ----------------------------------------------------------------------------- "
+echo " "
+
+df -h /home
+
+echo " "
+echo " ----------------------------------------------------------------------------- "
+echo " "
+
+ls -l /home
+
+echo " "
+echo " ----------------------------------------------------------------------------- "
+echo " "
+
+pgrep ttmswap
+pgrep seapplet
+
+echo " "
+echo " ----------------------------------------------------------------------------- "
+echo " "
+
+# ------------------------------------------------------------------------------------
+echo " "
+echo " ----------------------------------------------------------------------------- "
+echo " "
+# ------------------------------------------------------------------------------------
+# Exit with an explicit exit status last.
+echo "Script ran successfully with an exit status of 0 (zero)."
+echo " "
+exit 0
